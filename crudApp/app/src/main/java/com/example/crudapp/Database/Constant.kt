91f61789afe79ad69ed7ba4bc9135057674b375a
alfianbayu11 <alfianbayu11@gmail.com>
2020-12-17 13:01:22 +0700

    yes

--- crudApp/app/src/main/java/com/example/crudapp/Database/Constant.kt
+++
@@ -1,9 +0,0 @@
-package com.example.uas_mp.Database
-
-class Constant {
-    companion object {
-        const val TYPE_READ = 0
-        const val TYPE_CREATE = 1
-        const val TYPE_UPDATE = 2
-    }
-}
